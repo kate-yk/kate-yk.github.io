cf5453294e152d8f2a0ac4722330f7c619aabdef
JaehoonSong12 <manual20151276@gmail.com>
2025-08-17 23:24:29 -0400

    Normal Execution commit: 2025-08-17_23-24-29 updated by Jaehoon

--- docs_backup/controller.js
+++
@@ -1,9 +0,0 @@
-// Main Controller - Imports base functionality
-import { initializeBaseController } from './core-module.js';
-
-// Initialize base functionality
-initializeBaseController();
-
-/////////////////////////////////////////////
-// Add any page-specific functionality here
-/////////////////////////////////////////////
