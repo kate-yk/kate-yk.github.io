efa0a6d58edee9a43f46a362b72b5f3878297437
JaehoonSong12 <manual20151276@gmail.com>
2025-09-25 19:43:48 -0400

    Normal Execution commit: 2025-09-25_19-43-48 updated by Jaehoon

--- JSvanilla/src/js/controller/index.js
+++ JSvanilla/src/js/controller/index.js
@@ -10,7 +10,7 @@ export default function initIndex() {
   Util.renderParagraph('This content was rendered by core-module.js and invoked from pages/index.js.');
 
   // Load footer fragment and then setup notification system
-  Util.loadFragment('#footer', './footer.html').then(() => {
+  Util.loadFragment('#footer', 'footer.html').then(() => {
     // Setup notification system after footer is loaded
     Util.setupNotificationSystem();
   });
